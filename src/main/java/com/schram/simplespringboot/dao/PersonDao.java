ca4dcd67964a2f1dec9b4013793261aafaf09436
Giovani Schram <schram.giovani@gmail.com>
2016-06-10 11:43:34 -0300

    Craindo queries para person.

--- src/main/java/com/schram/simplespringboot/dao/PersonDao.java
+++ src/main/java/com/schram/simplespringboot/dao/PersonDao.java
@@ -1,9 +1,14 @@
 package com.schram.simplespringboot.dao;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.schram.simplespringboot.model.Person;
 
 public interface PersonDao extends CrudRepository<Person, Long> {
 	
+	Collection<Person> findByName(String name);
+	Collection<Person> findByNameAndDocument(String name, String document);
+	Collection<Person> findByNameContaining(String name);
 }
